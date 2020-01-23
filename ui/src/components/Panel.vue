c39bb212884013d6eee5bd7444af69cf5d00dbe0
TheWolfOfWar Programmeur <pro.stevestrz@gmail.com>
2020-01-23 07:11:53 +0100

    first commit

---
+++ ui/src/components/Panel.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="panel">
+    <Tile title_p="Vos news"/>
+    <Tile title_p="Agenda"/>
+    <Tile title_p="Paramètres"/>
+  </div>
+</template>
+
+<script>
+import Tile from "./Tile";
+
+export default {
+  name: 'panel',
+  components: {
+    Tile
+  }
+}
+</script>
+
+<style>
+#panel{
+    display:flex;
+    flex-direction: row;
+    justify-content: center;
+}
+</style>
---
+++ ui/src/components/Tile.vue
@@ -0,0 +1,77 @@
+<template>
+  <div id="tile">
+    <div class="container">
+        <div class="top">
+            <div class="title">
+                {{title}}
+            </div>
+        </div>
+        <div class="middle">
+            
+        </div>
+        <div class="bottom">
+            
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'tile',
+  data(){
+      return{
+          title: this.title_p,
+      }
+  },
+  components: {
+    
+  },
+  props:['title_p', 'bg_color']
+}
+</script>
+
+<style>
+#tile{
+    display:flex;
+    flex-direction: row;
+    justify-content: center;
+    margin: 5px;
+}
+
+.container{
+    display:flex;
+    flex-direction: column;
+    height: 400px;
+    width: 250px;
+    background-color: rgb(101, 193, 221);
+    transition: width 0.15s, height 0.15s;
+    box-shadow: 5px 5px 10px grey;
+}
+.container:hover{
+    height: 410px;
+    width: 260px;
+}
+
+.top{
+    display:flex;
+    flex-direction: row;
+    flex:1;
+}
+.middle{
+    display:flex;
+    flex-direction: row;
+    flex:1;
+}
+.bottom{
+    display:flex;
+    flex-direction: row;
+    flex:1;
+}
+
+.title{
+    margin: 8%;
+    font-size: 20px;
+}
+</style>
